3b1221a36d088faf8dd20c7f82c0614707991419
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-08-20 13:52:51 -0400

    butane:0.13.1

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.butane
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-butane/actions/workflows/build.yml/badge.svg)
+
+Installs [butane](https://github.com/coreos/butane).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.butane
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/coreos/butane/releases/download
+
+# https://github.com/coreos/butane/releases/download/v0.13.1/butane-x86_64-unknown-linux-gnu
+
+dl()
+{
+    local ver=$1
+    local arch=$2
+    local os=$3
+    local dotexe=${4:-}
+    local platform="${arch}-${os}"
+    local lfile=$DIR/butane-${ver}-${platform}${dotexe}
+    local url=$MIRROR/v${ver}/butane-${platform}${dotexe}
+    if [ ! -e $lfile ]
+    then
+        wget -q -O $lfile $url
+    fi
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl_ver ()
+{
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver aarch64 unknown-linux-gnu
+    dl $ver ppc64le unknown-linux-gnu
+    dl $ver s390x unknown-linux-gnu
+    dl $ver x86_64 apple-darwin
+    dl $ver x86_64 pc-windows-gnu .exe
+    dl $ver x86_64 unknown-linux-gnu
+}
+
+dl_ver ${1:-0.13.1}
